bf702ea5bca9a3baccabca67e470f09a6d5e6a56
jangsanghyen <jsh732386@gmail.com>
2022-05-26 16:27:12 +0900

    feat terrafrom

--- tf/sns.tf
+++ tf/sns.tf
@@ -2,6 +2,6 @@ resource "aws_sns_topic" "DeliveryStatusChange" {
   name = "DeliveryStatusChange"
 }
 
-# resource "aws_sns_topic" "NotificationSend" {
-#   name = "NotificationSend"
-# }
+resource "aws_sns_topic" "NotificationSend" {
+  name = "NotificationSend"
+}
