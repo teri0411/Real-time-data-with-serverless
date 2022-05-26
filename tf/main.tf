9f1e80cfd2d570c22433911ec0d91458a0a9618a
KKHYeon <kang01019@gmail.com>
2022-05-26 15:30:37 +0900

    fix: fix s3 policy

--- tf/main.tf
+++ tf/main.tf
@@ -7,12 +7,37 @@ resource "aws_s3_bucket" "tfstate" {
   bucket = "terraform-backend-for-200ok"
 }
 
+resource "aws_s3_bucket_policy" "allow_access_from_Administrators" {
+  bucket = aws_s3_bucket.tfstate.id
+  policy = data.aws_iam_policy_document.allow_access_from_Administrators.json
+}
+
+data "aws_iam_policy_document" "allow_access_from_Administrators" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["889058321615"]
+    }
+
+    actions = [
+      "s3:ListBucket","s3:GetObject", "s3:PutObject"
+    ]
+
+    resources = [
+      aws_s3_bucket.tfstate.arn,
+      "${aws_s3_bucket.tfstate.arn}/*",
+    ]
+  }
+}
+
 resource "aws_s3_bucket_versioning" "versioning_example" {
   bucket = aws_s3_bucket.tfstate.id
   versioning_configuration {
     status = "Enabled"
   }
 }
+
+
 
 # DynamoDB for terraform state lock
 resource "aws_dynamodb_table" "terraform_state_lock" {
