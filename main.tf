6d9d3d17716a640aa951e4def13431622323452e
yyuni915 <56291567+yyuni915@users.noreply.github.com>
2022-05-30 18:40:36 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,18 +3,29 @@
 #
 # 1. Create a `main.tf` file in the root of this repository with the `remote` backend and one or more resources defined.
  # Example `main.tf`:
-    # The configuration for the `remote` backend.
+#     # The configuration for the `remote` backend.
+#     terraform {
+#       backend "remote" {
+#         # The name of your Terraform Cloud organization.
+#         organization = "example-org-eca7ff"
+
+#         # The name of the Terraform Cloud workspace to store Terraform state files in.
+#         workspaces {
+#           name = "devops-01-final-TeamC-Scenario-02"
+#         }
+#       }
+#     }
+
     terraform {
-      backend "remote" {
-        # The name of your Terraform Cloud organization.
+      cloud {
         organization = "example-org-eca7ff"
 
-        # The name of the Terraform Cloud workspace to store Terraform state files in.
         workspaces {
-          name = "devops-01-final-TeamC-Scenario-02"
+          name = "final-project-teamC"
         }
       }
     }
+
 
     # An example resource that does nothing.
     resource "null_resource" "example" {
