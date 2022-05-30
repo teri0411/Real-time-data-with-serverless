95bc2c9c46f2f64a56bbe7fa12d8699b4579b363
yyuni915 <56291567+yyuni915@users.noreply.github.com>
2022-05-30 17:05:12 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+
+# To use this workflow, you will need to complete the following setup steps.
+#
+# 1. Create a `main.tf` file in the root of this repository with the `remote` backend and one or more resources defined.
+ # Example `main.tf`:
+    # The configuration for the `remote` backend.
+    terraform {
+      backend "remote" {
+        # The name of your Terraform Cloud organization.
+        organization = "example-org-eca7ff"
+
+        # The name of the Terraform Cloud workspace to store Terraform state files in.
+        workspaces {
+          name = "devops-01-final-TeamC-Scenario-02"
+        }
+      }
+    }
+
+    # An example resource that does nothing.
+    resource "null_resource" "example" {
+      triggers = {
+        value = "A example resource that does nothing!"
+      }
+    }
