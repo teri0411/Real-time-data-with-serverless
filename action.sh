92f801f9d3f8f96fe5381c345d96ffd7cfb32496
teri0411 <goaheadman@naver.com>
2022-06-01 20:38:57 +0900

    action test

--- action.sh
+++ action.sh
@@ -1,8 +1,6 @@
 # 실행명령어 : source action.sh
 #!/bin/bash
-unset ENDPOINT_URL
-unset WEBSOCKET
-unset WEBSOCKET_URL
+
 cd lambda && cd Websocket
 #sls deploy #아무것도 없을때 이 명령어 사용 (처음배포해놔야 url잡을 수 있다.)
 export ENDPOINT_URL=$(aws lambda get-function-url-config --function-name websocket-app-dev-publish | jq .FunctionUrl -r)
