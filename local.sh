28c8fe601a3fa92b60c591ded67ec2b54765509f
teri0411 <goaheadman@naver.com>
2022-06-01 21:20:05 +0900

    action test

--- local.sh
+++ local.sh
@@ -3,6 +3,7 @@
 
 cd lambda && cd Websocket
 #sls deploy #아무것도 없을때 이 명령어 사용 (처음배포해놔야 url잡을 수 있다.)
+export AWS_DEFAULT_REGION=ap-northeast-2
 export ENDPOINT_URL=$(aws lambda get-function-url-config --function-name websocket-app-dev-publish | jq .FunctionUrl -r)
 echo $ENDPOINT_URL
 export WEBSOCKET=$(aws apigatewayv2 get-apis | jq .Items -r | jq -c '.[] | select(.Name | contains("websocket"))'.ApiEndpoint -r | sed -e "s/\$/\/dev/g")
