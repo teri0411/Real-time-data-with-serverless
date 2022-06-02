69d727826e543c62ad82b269f1cf1110d2cf0022
KKHYeon <kang01019@gmail.com>
2022-06-02 10:17:02 +0900

    feat: sns subscription

--- tf/parameter.tf
+++ tf/parameter.tf
@@ -2,11 +2,32 @@ resource "aws_ssm_parameter" "sns_ChangeDeliveryStatus" {
   name  = "/200OK/SNS/DeliveryStatus"
   type  = "String"
   value = aws_sns_topic.DeliveryStatusChange.arn
+  overwrite = true
 }
 resource "aws_ssm_parameter" "kinesis_data_stream" {
   name  = "/200OK/Kinesis/Data_Stream_Name"
   type  = "String"
   value = aws_kinesis_stream.location_stream.name
+  overwrite = true
 }
 
-# hello?
+resource "aws_ssm_parameter" "opensearch_endpoint" {
+  name = "/200OK/Opensearch/Endpoint"
+  type = "String"
+  value = aws_opensearch_domain.delivery_cluster.endpoint
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "opensearch_arn" {
+  name = "/200OK/Opensearch/Arn"
+  type = "String"
+  value = aws_opensearch_domain.delivery_cluster.arn
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "sns_NotificationSend" {
+  name = "/200OK/SNS/NotificationSend"
+  type = "String"
+  value = aws_sns_topic.NotificationSend.arn
+  overwrite = true
+}
