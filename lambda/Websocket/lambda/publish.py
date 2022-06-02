f6d1a3d7dac53bc617e32afaf77f59d84e784270
teri0411 <goaheadman@naver.com>
2022-06-02 16:19:48 +0900

    action start

--- lambda/Websocket/lambda/publish.py
+++ lambda/Websocket/lambda/publish.py
@@ -11,55 +11,55 @@ def lambda_handler(event, context):
 
   print(event)
   print("----------event-----------")
-  
+  try:
 ######################################################
-  test = event["body"]
-  print('------------test----------------')
-  print(test)
+    test = event["body"]
+    print('------------test----------------')
+    print(test)
 
-  json_data = json.loads(test)
-  print('-----json_data---???')
-  print(json_data)
+    json_data = json.loads(test)
+    print('-----json_data---???')
+    print(json_data)
       
-  value = json_data["records"][0]
-  data = value["data"]
-  base64_bytes = data.encode('ascii')
-  decodedBytes = base64.b64decode(base64_bytes)
-  msg = decodedBytes.decode('ascii')
-  print("----msg----")
-  print(msg)
+    value = json_data["records"][0]
+    data = value["data"]
+    base64_bytes = data.encode('ascii')
+    decodedBytes = base64.b64decode(base64_bytes)
+    msg = decodedBytes.decode('ascii')
+    print("----msg----")
+    print(msg)
       
-  print('typetuype-----???????')
+    print('typetuype-----???????')
       
-  print(type(msg))
+    print(type(msg))
       
-  msg_data = json.loads(msg)
+    msg_data = json.loads(msg)
       
-  truckerId = msg_data["truckerId"]
+    truckerId = msg_data["truckerId"]
       
-  # truckerIds = msg["truckerId"]
-  print("---truckerId?????????????????")
-  print(truckerId)
+    # truckerIds = msg["truckerId"]
+    print("---truckerId?????????????????")
+    print(truckerId)
       
       
-  client = boto3.client(
-    'dynamodb',
-    aws_access_key_id= os.environ['aws_access_key_id'] ,
-    aws_secret_access_key= os.environ['aws_secret_key_id'],
-    )
-  dynamodb = boto3.resource(
-    'dynamodb',
-    aws_access_key_id= os.environ['aws_access_key_id'],
-    aws_secret_access_key= os.environ['aws_secret_key_id'],
-    )
-  table = dynamodb.Table('connection')
-  response = table.scan()
-  data2 = response['Items']
-  while 'truckerId' in response:
-    response = table.scan(ExclusiveStartKey=response['truckerId'])
-    data2.extend(response['Items'])
-  print('--data2-----------------------------')
-  print(data2)
+    client = boto3.client(
+      'dynamodb',
+      aws_access_key_id= os.environ['aws_access_key_id'] ,
+      aws_secret_access_key= os.environ['aws_secret_key_id'],
+      )
+    dynamodb = boto3.resource(
+      'dynamodb',
+      aws_access_key_id= os.environ['aws_access_key_id'],
+      aws_secret_access_key= os.environ['aws_secret_key_id'],
+      )
+    table = dynamodb.Table('connection')
+    response = table.scan()
+    data2 = response['Items']
+    while 'truckerId' in response:
+      response = table.scan(ExclusiveStartKey=response['truckerId'])
+      data2.extend(response['Items'])
+    print('--data2-----------------------------')
+    print(data2)
 
       
       # for 문으로 truckerId 전부 조회 != 일치하는 truckerId가 없을때, 
@@ -68,23 +68,24 @@ def lambda_handler(event, context):
       #r = table.get_item(truckerId=truckerId)
       #print('*******reponse- 있는데이터나와야함*****************************')
       #print(r)
-  clientApi = boto3.client('apigatewaymanagementapi', endpoint_url= os.environ['WEBSOCKET_URL'])
-  print(os.environ['ENDPOINT_URL'])
-  connectionId = response['Items'][0]["connectionId"]
+    clientApi = boto3.client('apigatewaymanagementapi', endpoint_url= os.environ['WEBSOCKET_URL'])
+    print(os.environ['ENDPOINT_URL'])
+    connectionId = response['Items'][0]["connectionId"]
     ##
-  result = table.query(
-        KeyConditionExpression=Key('truckerId').eq('588866')
+    result = table.query(
+          KeyConditionExpression=Key('truckerId').eq('588866')
       )
-  print("???result???")
-  print(result)
+    print("???result???")
+    print(result)
 
-  if result["Count"]>0:
-    clientApi.post_to_connection(Data=msg, ConnectionId=connectionId)
-    print(result["Count"])
-    print("진입!!!")
-  else:
+    if result["Count"]>0:
+      clientApi.post_to_connection(Data=msg, ConnectionId=connectionId)
+      print(result["Count"])
+      print("진입!!!")
+    else:
       print("아이템없어서 못줌")
-
+  except:
+    print("try 못함")
 
   response = requests.get(os.environ['WEBSOCKET_URL'])
   print("-----response")
