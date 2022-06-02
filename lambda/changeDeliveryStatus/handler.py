8be93d99fa8266ef644f307e307bfa11d3464fda
KKHYeon <kang01019@gmail.com>
2022-06-02 17:24:38 +0900

    chore: erase print

--- lambda/changeDeliveryStatus/handler.py
+++ lambda/changeDeliveryStatus/handler.py
@@ -12,6 +12,7 @@ def publishSNS(message):
         Message=message,
         MessageStructure='string'
     )
+    print(response)
     return response
     
 def changeStatus(status, id):
@@ -19,9 +20,7 @@ def changeStatus(status, id):
     region = "ap-northeast-2"
     service = 'es'
     credentials = boto3.Session().get_credentials()
-    print(0)
     awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
-    print(1)
     search = OpenSearch(
         hosts = [{'host': URL, 'port': 443}],
         http_auth = awsauth,
@@ -29,25 +28,20 @@ def changeStatus(status, id):
         verify_certs = True,
         connection_class = RequestsHttpConnection
     )
-    print(2)
     if status == 0:
         document = {"id": id, "status": "start"}
         response = search.index(index="status", id=id, body=document)
-        print(response)
         return response
     elif status == 1:
         document = {"id": id, "status": "end"}
         response = search.index(index="status", id=id, body=document)
-        print(response)
         return response
 
 def start(event, context):
     message = {"message": "start delivery"}
     id = event['body']
     id = json.loads(id)['id']
-    print(id)
     publishSNS(str(message)) #{"statusCode": 200, "body": message}
-    print(event)
     response = changeStatus(0, id)
     return response
 
