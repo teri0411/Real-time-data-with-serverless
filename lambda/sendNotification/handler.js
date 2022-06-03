9cfd2c681f8ffc2886f98089ca8faaa794720abd
teri0411 <goaheadman@naver.com>
2022-06-03 11:44:03 +0900

    action test

--- lambda/sendNotification/handler.js
+++ lambda/sendNotification/handler.js
@@ -27,6 +27,5 @@ module.exports.hello = async (event) => {
   //??  
   //? ?
   //???
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
